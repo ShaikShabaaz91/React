bce7f072862cd1cda358db7e40e573b1134379c7
Macbook <macbook@shabazz.local>
2024-01-01 23:58:39 +0530

    context api

---
+++ 07minicontext/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import UserContextProvider from "./Context/userContextProvider";
+function App() {
+  return (
+    <UserContextProvider>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
+}
+
+export default App;
